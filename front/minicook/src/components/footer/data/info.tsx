d02c2172ed4a132e670e6db323080e86b78d2d32
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2024-02-13 21:51:45 +0900

    design : #2 footer markup and css styling

---
+++ front/minicook/src/components/footer/data/info.tsx
@@ -0,0 +1,20 @@
+import { FaGithub } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
+import { RiKakaoTalkFill } from "react-icons/ri";
+
+const info = [
+  {
+    icon: <FaGithub />,
+    link: "https://github.com/OH-Neuri",
+  },
+  {
+    icon: <FaGoogle />,
+    link: "mailto:moho009804@gmail.com",
+  },
+  {
+    icon: <RiKakaoTalkFill />,
+    link: "https://open.kakao.com/o/skQu70Zf",
+  },
+];
+
+export default info;
