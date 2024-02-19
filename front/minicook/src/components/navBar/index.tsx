4aa49ea05ad05f4437a3b45265a990e96ba92f90
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2024-02-19 22:52:46 +0900

    feat : #4 상단바 검색창 마크업

--- front/minicook/src/components/navBar/index.tsx
+++ front/minicook/src/components/navBar/index.tsx
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import SearchBar from "../searchBar";
-import menu from "./data/menu";
+import tab from "./data/data";
 import { useNavigate } from "react-router-dom";
+import recipe from "../../data/recipe";
 const NavBar = () => {
+  const [navBarOpen, setNavBarOpen] = useState<boolean>(false);
+  const navbarInfoRef = useRef<HTMLDivElement | null>(null);
+  const [input, setInput] = useState<string>("");
   const navigate = useNavigate();
-
   const changePage = (e: React.MouseEvent<HTMLDivElement>) => {
     const page = e.currentTarget.dataset.id;
     navigate(`/${page}`);
   };
+
+  const handleOpenSearchBar = () => setNavBarOpen(true);
+  const handleCloseSearchBar = () => setNavBarOpen(false);
+  const handleInput = (newInput: string) => {
+    setInput(newInput);
+  };
+
+  useEffect(() => {
+    const handler = (e: { target: any }) => {
+      if (
+        navBarOpen &&
+        navbarInfoRef.current &&
+        !navbarInfoRef.current.contains(e.target)
+      ) {
+        handleCloseSearchBar();
+      }
+    };
+    document.addEventListener("click", handler);
+  }, [navBarOpen]);
 
   return (
     <>
       <NavBarWrapper>
         <div className='navbar-info'>
           <div className='navbar-list'>
-            {menu.map(({ title, link }) => (
+            {tab.map(({ title, link }) => (
               <div
                 className='navbar-items'
                 key={title}
@@ -26,7 +48,49 @@ const NavBar = () => {
               </div>
             ))}
           </div>
-          <SearchBar />
+          <div ref={navbarInfoRef}>
+            <SearchBar onChangeInput={handleInput} onSearchClick={handleOpenSearchBar} />
+            {navBarOpen && (
+              <div className='search-bar-info'>
+                <div className='search-bar-info-wrapper'>
+                  <div className='search-bar-info-recipe'>
+                    <div className='mb-1 font-black text-lg'>재료명</div>
+                    <div className='search-bar-info-List'>
+                      {recipe.map(
+                        ({ id, thumbnail, name, ingredients }) =>
+                          input &&
+                          ingredients.some((i) => new RegExp(input).test(i)) && (
+                            <div className='search-bar-info-item'>
+                              <div>
+                                <div className='font-extrabold text-base'>{`${name}`}</div>
+                                <div className='text'>{`재료 : ${ingredients}`}</div>
+                              </div>
+                            </div>
+                          )
+                      )}
+                    </div>
+                  </div>
+                  <div className='search-bar-info-menu'>
+                    <div className='mb-1 font-black text-lg'>요리명</div>
+                    <div className='search-bar-info-List'>
+                      {recipe.map(
+                        ({ id, thumbnail, name, ingredients }) =>
+                          input &&
+                          new RegExp(input).test(name) && (
+                            <div className='search-bar-info-item'>
+                              <div>
+                                <div className='font-extrabold text-base'>{`${name}`}</div>
+                                <div className='text'>{`재료 : ${ingredients}`}</div>
+                              </div>
+                            </div>
+                          )
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
           <div className='navbar-text'>#대파 #삼겹살 #김밥</div>
         </div>
       </NavBarWrapper>
@@ -41,7 +105,74 @@ const NavBarWrapper = styled.div`
   justify-content: center;
   border-top :  1.8px solid #fffbf5;
   border-bottom:  1.8px solid #fffbf5;
-  background-color: ${(props) => props.theme.colors.color1};
+  background-color:white;
+  position: relative;
+  .search-bar-info{
+    top:3rem;
+    height: 19rem;
+    background-color: #ffffff;
+    width: 60%;
+    position: absolute;
+    left: 23rem;
+    z-index: 10;
+    opacity: 0;
+    animation: fadeIn 0.2s ease-out forwards;
+    border-bottom: 2px solid #cdc7be;
+    display: flex;
+    justify-content: center;
+    .search-bar-info-wrapper{
+      padding: 0.5rem 0;
+      width: 60%;
+      display: flex;
+      justify-content: space-evenly;
+      .search-bar-info-recipe{
+        padding: 0.5rem 0.5rem;
+        overflow-y: scroll;
+        overflow-x: hidden; 
+    background-color: #f4efeb;
+        width: 45%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        scroll-behavior: auto;
+        .search-bar-info-item{
+          margin-bottom: 1rem;
+          display: flex;
+        }
+      }
+      .search-bar-info-recipe::-webkit-scrollbar-thumb{
+        background: none
+      }
+      .search-bar-info-menu{
+        padding: 0.5rem 0.5rem;
+        overflow-y: scroll;
+        overflow-x: hidden; 
+    background-color: #f4efeb;
+
+        width: 43%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        .search-bar-info-item{
+          margin-bottom: 1rem;
+          display: flex;
+        }
+      }
+      .search-bar-info-menu::-webkit-scrollbar-thumb{
+        background: none
+      }
+    }
+  }
+   @keyframes fadeIn {
+    from {
+      opacity: 0;
+      transform: translateY(-10px); /* 시작 시 상단에서 -10px 이동 */
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0); /* 끝날 때 이동하지 않음 */
+    }
+  }
 
   .navbar-info{
     width: 60%;
--- front/minicook/src/components/searchBar/index.tsx
+++ front/minicook/src/components/searchBar/index.tsx
@@ -1,38 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { IoSearchOutline } from "react-icons/io5";
 
-const SearchBar = () => {
+interface SearchBarProps {
+  onSearchClick: () => void;
+  onChangeInput: (newInput: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearchClick, onChangeInput }) => {
+  const handleSearchBar = (e: React.MouseEvent<HTMLDivElement>) => {
+    onSearchClick();
+  };
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChangeInput(e.target.value);
+  };
+
   return (
-    <SearchBarWrapper className='cursor-pointer'>
+    <SearchBarWrapper className='cursor-pointer' onClick={handleSearchBar}>
       <div className='search-icon focus:hidden'>
         <IoSearchOutline size={22} />
       </div>
-      <div className='search-input-wrapper'>
+      <div className='search-input-wrapper' onClick={(e) => handleSearchBar(e)}>
         <input
           className='search-input'
-          placeholder='재료, 요리명 등 다양하게 검색해보세요! (예: 마늘)'></input>
+          placeholder='재료, 요리명 등 다양하게 검색해보세요! (예: 마늘)'
+          onChange={handleInput}></input>
       </div>
+      <div className='search-list'></div>
     </SearchBarWrapper>
   );
 };
 
 const SearchBarWrapper = styled.div`
- height: 2.2rem;
+ height: 2.4rem;
  display: flex;
  width: 22rem;
- background-color:#fcfcfc;
- border : 1px solid #d3bea7;
- border-radius: 25px;
+ background-color:#fefdfc;
+ border-bottom : 2px solid #eee0d0;
+ border-radius: 15px;
  display: flex;
  align-items: center;
  padding:0 0.8rem;
+ position:relative;
 
  .search-input-wrapper{
   display: flex;
   justify-content: center;
   width: 18rem;
  }
+
  .search-input{
   font-size: smaller;
   background-color:#fcfcfc;
@@ -40,10 +57,17 @@ const SearchBarWrapper = styled.div`
   outline: none;
   width: 20rem;
  }
+
  .search-input::placeholder{
   font-size: small;
   color: #444444
  }
 
+.search-list{
+  z-index: 10;
+  position: absolute;
+
+}
 `;
+
 export default SearchBar;
