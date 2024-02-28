d9b9ed01200acbf4462cc616ea576858b8bd492b
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2024-02-28 23:18:56 +0900

    feat : #7 모달창에서 재료 배열 상태 업데이트 처리

--- front/minicook/src/pages/recipe/select/index.tsx
+++ front/minicook/src/pages/recipe/select/index.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import styled from "styled-components";
+import { produce } from "immer";
+import SelectButton from "../../../components/selectButton";
 
 const RecipeSelect = () => {
-  return <div>recipeSelect</div>;
+  const [selectedIngredients, setSelectedIngredients] = useState<string[]>([]);
+
+  const handleSelectedIngredient = useCallback(
+    (ingredientName: string) => {
+      //const ingredient = e.target
+      setSelectedIngredients((prev) =>
+        produce(prev, (draft) => {
+          const index = draft.indexOf(ingredientName);
+          if (index == -1) {
+            draft.push(ingredientName);
+          } else {
+            draft.splice(index, 1);
+          }
+        })
+      );
+    },
+    [selectedIngredients]
+  );
+
+  return (
+    <RecipeSelectWrapper>
+      <SelectButton onSelect={handleSelectedIngredient} />
+      {/*<SelectdButtonList onSelect={handleSelectedIngredient} />
+      <RecipeSelect selected={selectedIngredients} />*/}
+    </RecipeSelectWrapper>
+  );
 };
+
+const RecipeSelectWrapper = styled.div`
+background-color: #F4F1EB;
+width:100%;
+height: 75vh;
+padding: 3rem 30% 8rem 30%;
+`;
 
 export default RecipeSelect;
