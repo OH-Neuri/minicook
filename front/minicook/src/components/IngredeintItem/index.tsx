61bd2e9c6d0c72cf4782bef45ce7654d460fb49e
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2024-03-04 19:12:00 +0900

    feat : #7 재료 선택 모달창 버튼 styling

--- front/minicook/src/components/IngredeintItem/index.tsx
+++ front/minicook/src/components/IngredeintItem/index.tsx
@@ -4,11 +4,19 @@ import { IngredientType } from "../../pages/recipe/select/data/ingredients";
 
 interface IngredientItemProps {
   ingredient: IngredientType;
-  onSelect: (e: string) => void;
+  onSelect: (ingredientName: string, categoryNumber: number) => void;
   onToggle: (id: number, name: string) => void;
   id?: string;
 }
-
+/**
+ * IngredientItem 컴포넌트
+ *
+ * 재료 선택 모달창 안에 있는 Item 버튼을 나타냅니다.
+ * @prop {IngredientType} ingredient - 재료의 제목과 체크 상황을 저장하고 있는 재료 객체
+ * @prop {funcion} onSelect - 사용자가 선택한 재료 배열의 상태를 업데이트 하는 함수
+ * @prop {funtion} onToggle - 재료 정보 배열 상태에서 재료의 클릭 상태를 업데이트 하는 함수
+ * @prop {string} id - 카테고리 번호
+ */
 const IngredientItem: React.FC<IngredientItemProps> = ({
   onSelect,
   onToggle,
@@ -17,17 +25,17 @@ const IngredientItem: React.FC<IngredientItemProps> = ({
 }) => {
   const { name, checked } = ingredient;
   const handleToggle = () => {
-    name && onSelect(name);
+    name && id && onSelect(name, parseInt(id));
     id && onToggle(parseInt(id), name);
   };
 
   return (
-    <IngredientItemWrapper isChecked={checked} onClick={handleToggle}>
+    <IngredientItemWrapper $isChecked={checked} onClick={handleToggle}>
       <div className='item'>{name}</div>
     </IngredientItemWrapper>
   );
 };
-const IngredientItemWrapper = styled.div<{ isChecked: boolean }>`
+const IngredientItemWrapper = styled.div<{ $isChecked: boolean }>`
     .item{
         padding: 2px 2px;
         display: flex;
@@ -35,12 +43,22 @@ const IngredientItemWrapper = styled.div<{ isChecked: boolean }>`
         align-items: center;
         width: 6.6rem;
         height: 2.8rem;
-        background-color: white;
         margin-right: 0.5rem;
         border-radius: 5px;
         border: 1px solid #dddddd;
+        color: white;
+        font-weight: 600;
         ${(props) =>
-          props.isChecked ? css`background-color:#bacfc4;` : css`background-color:white;`}
+          props.$isChecked
+            ? css`
+            background-color:#3c721d;
+            border-bottom : 6px solid #aedd02 ;
+            `
+            : css`
+            background-color:#85a996;
+            border-bottom:5px solid #d3ddd7;
+            border: 2px solid #66716b
+            `}
     &:hover{
         cursor: pointer;
     }
