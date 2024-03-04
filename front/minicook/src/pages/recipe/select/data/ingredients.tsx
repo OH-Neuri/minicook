541709f68d011a92c3aea846726c7be31eb60a99
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2024-03-04 19:19:39 +0900

    feat : #13 재료 상태 배열 타입 변경

--- front/minicook/src/pages/recipe/select/data/ingredients.tsx
+++ front/minicook/src/pages/recipe/select/data/ingredients.tsx
@@ -1,92 +1,115 @@
-interface IngredientsMenu {
-  id: number;
-  category: string;
-  ingredients: string[];
+export interface SelectedIngredients {
+  name: string;
+  category: number;
 }
 
-const ingredientsMenu: IngredientsMenu[] = [
+export interface IngredientsMenuType {
+  id: number;
+  category: string;
+  ingredients: IngredientType[];
+}
+export interface IngredientType {
+  name: string;
+  checked: boolean;
+}
+
+const ingredientsMenu: IngredientsMenuType[] = [
   {
     id: 0,
     category: "채소류",
     ingredients: [
-      "대파",
-      "양파",
-      "마늘",
-      "배추",
-      "청양고추",
-      "느타리버섯",
-      "애호박",
-      "당근",
-      "가지",
-      "부추",
-      "양배추",
-      "상추",
-      "시금치",
-      "무",
-      "우엉",
-      "오이",
-      "피망",
-      "브로콜리",
+      { name: "대파", checked: false },
+      { name: "양파", checked: false },
+      { name: "마늘", checked: false },
+      { name: "배추", checked: false },
+      { name: "청양고추", checked: false },
+      { name: "느타리버섯", checked: false },
+      { name: "애호박", checked: false },
+      { name: "당근", checked: false },
+      { name: "가지", checked: false },
+      { name: "부추", checked: false },
+      { name: "양배추", checked: false },
+      { name: "상추", checked: false },
+      { name: "시금치", checked: false },
+      { name: "무", checked: false },
+      { name: "우엉", checked: false },
+      { name: "오이", checked: false },
+      { name: "피망", checked: false },
+      { name: "브로콜리", checked: false },
     ],
   },
   {
     id: 1,
     category: "과일류",
     ingredients: [
-      "매실",
-      "배",
-      "사과",
-      "복숭아",
-      "자두",
-      "대추",
-      "포도",
-      "파인애플",
-      "딸기",
-      "밤",
-      "은행",
+      { name: "매실", checked: false },
+      { name: "배", checked: false },
+      { name: "사과", checked: false },
+      { name: "복숭아", checked: false },
+      { name: "자두", checked: false },
+      { name: "대추", checked: false },
+      { name: "포도", checked: false },
+      { name: "파인애플", checked: false },
+      { name: "딸기", checked: false },
+      { name: "밤", checked: false },
+      { name: "은행", checked: false },
     ],
   },
   {
     id: 2,
     category: "육류",
-    ingredients: ["닭", "쇠고기", "돼지고기", "차돌박이", "삼겹살", "목살"],
+    ingredients: [
+      { name: "닭", checked: false },
+      { name: "쇠고기", checked: false },
+      { name: "돼지고기", checked: false },
+      { name: "차돌박이", checked: false },
+      { name: "삼겹살", checked: false },
+      { name: "목살", checked: false },
+    ],
   },
   {
     id: 3,
     category: "우유 및 유제품",
-    ingredients: ["우유", "산양유", "분유", "버터", "치즈", "요구르트"],
+    ingredients: [
+      { name: "우유", checked: false },
+      { name: "산양유", checked: false },
+      { name: "분유", checked: false },
+      { name: "버터", checked: false },
+      { name: "치즈", checked: false },
+      { name: "요구르트", checked: false },
+    ],
   },
   {
     id: 4,
     category: "어류 및 패류",
     ingredients: [
-      "고등어",
-      "새우",
-      "오징어",
-      "홍합",
-      "게맛살",
-      "굴",
-      "게",
-      "조개",
-      "전복",
+      { name: "고등어", checked: false },
+      { name: "새우", checked: false },
+      { name: "오징어", checked: false },
+      { name: "홍합", checked: false },
+      { name: "게맛살", checked: false },
+      { name: "굴", checked: false },
+      { name: "게", checked: false },
+      { name: "조개", checked: false },
+      { name: "전복", checked: false },
     ],
   },
   {
     id: 5,
     category: "기타",
     ingredients: [
-      "간장",
-      "소금",
-      "물",
-      "후추",
-      "미향",
-      "참기름",
-      "카놀라유",
-      "찹쌀가루",
-      "아이올리소스",
-      "케찹",
-      "돈까스소스",
-      "딸기잼",
+      { name: "간장", checked: false },
+      { name: "소금", checked: false },
+      { name: "물", checked: false },
+      { name: "후추", checked: false },
+      { name: "미향", checked: false },
+      { name: "참기름", checked: false },
+      { name: "카놀라유", checked: false },
+      { name: "찹쌀가루", checked: false },
+      { name: "아이올리소스", checked: false },
+      { name: "케찹", checked: false },
+      { name: "돈까스소스", checked: false },
+      { name: "딸기잼", checked: false },
     ],
   },
 ];
