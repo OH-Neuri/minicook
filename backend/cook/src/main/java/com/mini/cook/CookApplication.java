f7e4eeae824152fa7c1561499d661ddec80701fe
오종석 <hynb14@gmail.com>
2024-03-10 14:29:21 +0900

    backend init

---
+++ backend/cook/src/main/java/com/mini/cook/CookApplication.java
@@ -0,0 +1,13 @@
+package com.mini.cook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookApplication.class, args);
+	}
+
+}
---
+++ backend/cook/src/test/java/com/mini/cook/CookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini.cook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
