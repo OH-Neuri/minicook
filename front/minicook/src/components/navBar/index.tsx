f12ac6565c7e7963c36873c12446792c16d55434
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2024-03-24 23:14:35 +0900

    feat : #38 NavBar 검색바 컴포넌트 경로 수정

--- front/minicook/src/components/navBar/index.tsx
+++ front/minicook/src/components/navBar/index.tsx
@@ -1,22 +1,30 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import SearchBarInput from "../search/searchBarInput";
 import { Link } from "react-router-dom";
 import recipe from "../../data/recipe";
 import SearchBarInfo from "../search/searchBarInfo";
+import useDebounce from "../../hooks/useDebounce";
+import SearchBarInput from "../search/searchBarInput";
 
-const NavBar = () => {
-  const [navBarOpen, setNavBarOpen] = useState<boolean>(false);
-  const navbarInfoRef = useRef<HTMLDivElement | null>(null);
-  const [input, setInput] = useState<string>("");
+const NavBar: React.FC = () => {
+  const [navBarOpen, setNavBarOpen] = useState<boolean>(false); // 검색 바의 열림/닫힘 상태를 관리하는 상태 변수
+  const navbarInfoRef = useRef<HTMLDivElement | null>(null); // 검색 바의 정보를 참조하는 ref
+  const [input, setInput] = useState<string>(""); // 검색어를 관리하는 상태 변수
+  const debounceInput = useDebounce(input, 1000); // 검색어 입력의 디바운스를 적용한 값
 
-  const handleOpenSearchBar = () => setNavBarOpen(true);
-  const handleCloseSearchBar = () => setNavBarOpen(false);
+  const handleOpenSearchBar = () => setNavBarOpen(true); // 검색 바 열기 이벤트 핸들러
+  const handleCloseSearchBar = () => setNavBarOpen(false); // 검색 바 닫기 이벤트 핸들러
 
-  const handleInput = useCallback((newInput: string) => {
-    setInput(newInput);
+  const handleInput = useCallback((input: string) => {
+    setInput(input);
   }, []);
 
+  // 검색어가 변경될 때마다 API 요청을 실행
+  useEffect(() => {
+    //console.log("api요청");
+  }, [debounceInput]);
+
+  // 검색바가 열려 있고 외부를 클릭할 때 검색바를 닫음
   useEffect(() => {
     const handler = (e: { target: any }) => {
       if (
@@ -98,5 +106,4 @@ const StyledText = styled.div`
 }
 `;
 
-
-export default NavBar;
+export default React.memo(NavBar);
