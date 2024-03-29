c0be216a2a69233c36063bd80a36b48acc5f3521
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2024-03-29 15:54:52 +0900

    feat : #29 axios baseURL 수정

--- front/minicook/src/api/index.ts
+++ front/minicook/src/api/index.ts
@@ -1,12 +1,13 @@
 import axios, { AxiosInstance } from "axios";
 import { refreshToken } from "./auth/refreshToken";
 
-export const BACKEND_URL = "https://";
+export const BACKEND_URL = "http://localhost:8080/api/";
 
 const accessToken =
   typeof window !== "undefined" ? sessionStorage.getItem("accessToken") : null;
 
-// 로그인 후 사용할 axios (토큰이 필요한 경우)
+// 로그인 후 사용할 axios객체 (토큰이 필요한 경우)
+// baseURL, header 설정
 export const authClient: AxiosInstance = axios.create({
   baseURL: `${BACKEND_URL}`,
   headers: {
@@ -14,13 +15,10 @@ export const authClient: AxiosInstance = axios.create({
   },
 });
 
+// 브라우저가 HTTP 요청을 할 때 해당 요청에 쿠키와 인증 정보를 함께 보냄
 authClient.defaults.withCredentials = true;
 
-// 로그인이 필요없는 axios
-export const defaultClient: AxiosInstance = axios.create({
-  baseURL: `${BACKEND_URL}`,
-});
-
+// 응답코드가 403일 경우 토큰 재발급
 authClient.interceptors.response.use(
   (res) => res,
   (err) => {
@@ -28,3 +26,9 @@ authClient.interceptors.response.use(
     if (err.response.status === 403) refreshToken(`${BACKEND_URL}/user/auth`);
   }
 );
+
+// 로그인이 필요없는 axios객체 (토큰이 필요없는 경우)
+// baseURL 설정
+export const defaultClient: AxiosInstance = axios.create({
+  baseURL: `${BACKEND_URL}`,
+});
