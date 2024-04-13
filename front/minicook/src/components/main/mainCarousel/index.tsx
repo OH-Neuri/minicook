9ce81ab31c600b085dce61462c6bff65f5e09148
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2024-04-13 15:03:41 +0900

    feat : #46 메인 페이지 반응형 웹 캐러셀 제작

--- front/minicook/src/components/main/mainCarousel/index.tsx
+++ front/minicook/src/components/main/mainCarousel/index.tsx
@@ -1,147 +1,146 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
 import { BsChevronLeft } from "@react-icons/all-files/bs/BsChevronLeft";
 import { BsChevronRight } from "@react-icons/all-files/bs/BsChevronRight";
 import { FaCircle } from "@react-icons/all-files/fa/FaCircle";
 import styled from "styled-components";
 import { RecipeType } from "../../../type";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper/modules";
+import { Pagination } from "swiper/modules";
+import SwiperCore from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 interface MainCarouselProps {
   recipe: RecipeType[];
 }
 const MainCarousel: React.FC<MainCarouselProps> = ({ recipe }) => {
-  const [index, setIndex] = useState<number>(0);
-  const carouselImageRef = useRef<HTMLDivElement | null>(null);
-
-  const prevClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    if (index === 0) return;
-    setIndex(index - 1);
-  };
-
-  const nextClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    if (index === 3) return;
-    setIndex(index + 1);
-  };
-
-  useEffect(() => {
-    if (carouselImageRef.current != null) {
-      carouselImageRef.current.style.transform = `translateX(-${index * 580}px)`;
-    }
-  }, [index]);
+  SwiperCore.use([Autoplay, Navigation]);
+  const sliderImages = [
+    "https://ottogi.okitchen.co.kr/pds/editor/30_1.jpg",
+    "https://ottogi.okitchen.co.kr/pds/editor/30_2.jpg",
+    "https://ottogi.okitchen.co.kr/pds/editor/30_3.jpg",
+    "https://ottogi.okitchen.co.kr/pds/editor/30_4.jpg",
+  ];
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
 
   return (
     <MainCarouselWrapper>
-      <div className='text'>
-        <div className='text-top'>
-          내게 딱 __ 맞는
-          <br />
-          레시피 찾기
-        </div>
-        <div className='text-bottom'>
-          카테고리 별로 원하는 레시피를
-          <br />
-          추천받을 수 있어요.
-        </div>
-      </div>
-
-      <div className='carousel-wrapper'>
-        <StyledButton aria-label='Carousel Left' onClick={(e) => prevClick(e)}>
-          <BsChevronLeft size={20} />
-        </StyledButton>
-        <Carousel>
-          <div className='carousel-image' ref={carouselImageRef}>
-            {recipe.slice(0, 4).map(({ id, thumbnail }) => (
-              <img className='rounded-xl' key={id} src={thumbnail} alt='Recipe' />
-            ))}
+      <div className='section'>
+        <div className='text'>
+          <div className='text-top'>
+            내게 딱 __ 맞는
+            <br />
+            레시피 찾기
           </div>
-          <div className='carousel-indicator'>
-            {[0, 1, 2, 3].map((v, i) => (
-              <CircleIcon key={i} $active={index === v} />
-            ))}
+          <div className='text-bottom'>
+            <span>카테고리 별로 원하는 레시피를</span>
+            <br />
+            <span>추천 받을 수 있어요.</span>
           </div>
-        </Carousel>
-        <StyledButton aria-label='Carousel Right' onClick={(e) => nextClick(e)}>
-          <BsChevronRight size={20} />
-        </StyledButton>
+        </div>
+        <Swiper
+          loop={true}
+          slidesPerView={1}
+          pagination={{ dynamicBullets: true }}
+          modules={[Pagination, Navigation, Autoplay]}
+          autoplay={{
+            delay: 4000,
+            disableOnInteraction: false,
+          }}
+          onInit={(swiper: any) => {
+            swiper.params.navigation.prevEl = prevRef.current;
+            swiper.params.navigation.nextEl = nextRef.current;
+            swiper.navigation.init();
+            swiper.navigation.update();
+          }}
+          className='mySwiper'>
+          {sliderImages.map((value, index) => {
+            return (
+              <SwiperSlide>
+                <img
+                  key={index}
+                  src={`${value}`}
+                  className='object-contain w-50 h-50'
+                  alt='Carousel'
+                />
+              </SwiperSlide>
+            );
+          })}
+          <div className='absolute flex justify-around w-full'>
+            <StyledButton ref={prevRef} aria-label='Carousel Left Button'>
+              <BsChevronLeft size={20} />
+            </StyledButton>
+            <StyledButton ref={nextRef} aria-label='Carousel Right Button'>
+              <BsChevronRight size={20} />
+            </StyledButton>
+          </div>
+        </Swiper>
       </div>
     </MainCarouselWrapper>
   );
 };
 
 const MainCarouselWrapper = styled.div`
+  width: 100%;
+  height: 800px;
+  padding: 90px 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${(props) => props.theme.colors.bgColor};
+
+  @media screen and (max-width: 980px) {
+      max-height: 700px;
+  }
+
+  .section{
     width: 100%;
-    height: 800px;
-    padding: 90px 10px;
+    max-width: 1000px;
     display: flex;
-    justify-content: center;
     align-items: center;
-
-    background-color: ${(props) => props.theme.colors.bgColor};
-
-    @media screen and (max-width: 980px) {
-        height: auto;
-        flex-wrap: wrap;    
-        max-height: 750px;
-    }
-
     .text{
-    line-height: 3rem;
-        .text-top{
-            font-weight: 900;
-            font-size: 1.5rem/* 24px */;
-            line-height: 2rem/* 32px */;
-        }
-        .text-bottom{
-            line-height: 2rem/* 32px */;
-            --tw-text-opacity: 1;
-            color: rgb(156 163 175 / var(--tw-text-opacity));
-        }
-    }
-
-    .carousel-wrapper{
-        width: 750px;
-        height: 60%;
+      line-height: 3rem;
+      @media screen and (max-width: 980px) {
+        width: 100%;
         display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 0 10px;
-        margin-top:30px;
-
+        flex-direction:column;
+        margin-bottom: 20px;
+        }
+      .text-top{
+        font-weight: 900;
+        font-size: 1.5rem/* 24px */;
+        line-height: 2rem/* 32px */;
         @media screen and (max-width: 980px) {
-        width:100%;
-        height: auto;
+          text-align: center;
         }
+      }
+      .text-bottom{
+          line-height: 2rem/* 32px */;
+          --tw-text-opacity: 1;
+          color: rgb(156 163 175 / var(--tw-text-opacity));
+        @media screen and (max-width: 980px) {
+          text-align: center;
+        }
+      }
     }
-`;
+  
+    .mySwiper{
+      width: 75%;
+      max-width: 600px;
+      --swiper-theme-color: #2d7700;
+    }
+      @media screen and (max-width: 980px) {
+      flex-wrap: wrap;    
+      max-height: 750px;
+  }
+  }
 
-const Carousel = styled.div`
-      width: 570px;
-      height: 440px;
-      margin: 0 15px;
-      display: flex;
-      flex-direction: column;
-      justify-content: start;
-      align-items: center;
-      overflow: hidden;
-      background-size: cover;
-      object-fit: cover;
-    @media screen and (max-width: 980px) {
-        width: 100%;
-        height: auto;
-        max-width: 570px;
-    }
-      .carousel-image{
-        width: 100%;
-        height: 100%;
-        padding-left: 3px;
-        max-height: 410px;
-        display: flex;
-        transform: translateX(0px);
-        transition : transform 0.2s;
-      }
-      .carousel-indicator{
-        display: flex;
-        margin-top: 10px;
-      }
+
 `;
 
 const StyledButton = styled.button`
